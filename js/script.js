0423c7c3ac67b2275fd95e4d688b0bd7502317dd
Venika Sem <venika.sem@mths.ca>
2023-01-23 04:18:04 +0000

    fixed indenting

--- js/script.js
+++ js/script.js
@@ -14,35 +14,35 @@ var both = 0;
 
 // move left function
 function moveLeft(){
-    var left =
-    parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-    if(left>0){
-        character.style.left = left - 2 + "px";
-    }
+  var left =
+  parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+  if(left>0){
+      character.style.left = left - 2 + "px";
+  }
 }
 
 // move right function
 function moveRight(){
-    var left =
-    parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-    if(left<380){
-        character.style.left = left + 2 + "px";
-    }
+  var left =
+  parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+  if(left<380){
+      character.style.left = left + 2 + "px";
+  }
 }
 
 document.addEventListener("keydown", event => {
-    if(both==0){
-        both++;
-        if(event.key==="ArrowLeft"){
-            interval = setInterval(moveLeft, 1);
-        }
-        if(event.key==="ArrowRight"){
-            interval = setInterval(moveRight, 1);
-        }
-    }
+  if(both==0){
+      both++;
+      if(event.key==="ArrowLeft"){
+          interval = setInterval(moveLeft, 1);
+      }
+      if(event.key==="ArrowRight"){
+          interval = setInterval(moveRight, 1);
+      }
+  }
 });
 
 document.addEventListener("keyup", event => {
-    clearInterval(interval);
-    both=0;
+  clearInterval(interval);
+  both=0;
 });
