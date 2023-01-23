04bb13e17b3e3ffb08a25a723ee58a823aea9739
Venika Sem <venika.sem@mths.ca>
2023-01-23 15:17:30 +0000

    move ball

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ var game = document.getElementById("game");
 var interval;
 var both = 0;
 var counter = 0;
+var currentBlocks = [];
 
 // move left function
 function moveLeft(){
@@ -59,19 +60,29 @@ setInterval(function () {
   var holeLastTop =
     parseInt(window.getComputedStyle(holeLast).getPropertyValue("top"));
   }
-  if(blockLastTop<400||counter==0){
-  var block = document.createElement("div");
-  var hole = document.createElement("div");
-  block.setAttribute("class", "block");
-  hole.setAttribute("class", "hole");
-  block.setAttribute("id", "block"+counter);
-  hole.setAttribute("id", "hole" + counter);
-  block.style.top = blockLastTop + 100 + "px";
-  hole.style.top = holeLastTop + 100 + "px";
-  var random = Math.floor(Math.random() * 360);
-  hole.style.left = random + "px";
-  game.appendChild(block);
-  game.appendChild(hole);
-  counter++;
+  if (blockLastTop < 400 || counter == 0) {
+    var block = document.createElement("div");
+    var hole = document.createElement("div");
+    block.setAttribute("class", "block");
+    hole.setAttribute("class", "hole");
+    block.setAttribute("id", "block"+counter);
+    hole.setAttribute("id", "hole" + counter);
+    block.style.top = blockLastTop + 100 + "px";
+    hole.style.top = holeLastTop + 100 + "px";
+    var random = Math.floor(Math.random() * 360);
+    hole.style.left = random + "px";
+    game.appendChild(block);
+    game.appendChild(hole);
+    currentBlocks.push(counter);
+    counter++;
+  }
+  for (var i = 0; i < currentBlocks.length; i++) {
+    let current = currentBlocks[i];
+    let iblock = document.getElementById("block"+current);
+    let ihole = document.getElementById("hole" + current);
+    let iblockTop =
+    parseFloat(window.getComputedStyle(iblock).getPropertyValue("top"));
+    iblock.style.top = iblockTop - 0.5 + "px";
+    ihole.style.top = iblockTop - 0.5 + "px";
   }
 }, 1);
